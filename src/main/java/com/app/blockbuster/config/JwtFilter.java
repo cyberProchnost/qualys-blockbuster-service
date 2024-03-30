900f4c3a837ea95700c73e9b07fa744ecbdfc47c
Rajkumar Mokal <raj143mokal@gmail.com>
2024-03-30 11:37:02 +0530

    update authentication, add swagger

--- src/main/java/com/app/blockbuster/config/JwtFilter.java
+++ src/main/java/com/app/blockbuster/config/JwtFilter.java
@@ -2,9 +2,13 @@ package com.app.blockbuster.config;
 
 import com.app.blockbuster.entity.User;
 import com.app.blockbuster.enums.Role;
+import com.app.blockbuster.exception.ErrorMessage;
 import com.app.blockbuster.exception.InvalidAuthenticationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -14,6 +18,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 public class JwtFilter extends GenericFilterBean {
     @Override
@@ -21,14 +27,46 @@ public class JwtFilter extends GenericFilterBean {
         final HttpServletRequest request = (HttpServletRequest) servletRequest;
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
         final String authHeader = request.getHeader("authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer "))
-            throw new InvalidAuthenticationException("Invalid Or No Authentication token");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            try {
+                throw new InvalidAuthenticationException("Invalid Or No Authentication token");
+            } catch (InvalidAuthenticationException ex) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                ErrorMessage errorMessage = ErrorMessage.builder().
+                        statusCode(HttpStatus.UNAUTHORIZED.value()).
+                        timestamp(new Date()).message(ex.getMessage()).build();
+                PrintWriter writer = response.getWriter();
+                writer.write(convertObjectToJson(errorMessage));
+                writer.flush();
+                return;
+            }
+        }
         final String token = authHeader.substring(7);
-        Claims claims = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
+        Claims claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ErrorMessage errorMessage = ErrorMessage.builder().
+                    statusCode(HttpStatus.UNAUTHORIZED.value()).
+                    timestamp(new Date()).message("Token expired or invalid token").build();
+            PrintWriter writer = response.getWriter();
+            writer.write(convertObjectToJson(errorMessage));
+            writer.flush();
+            return;
+        }
         String[] userDetails = claims.getSubject().split("::");
         User user = User.builder().email(userDetails[0]).name(userDetails[1]).
                 role(userDetails[2].equals("ADMIN") ? Role.ADMIN : Role.USER).id(Long.parseLong(userDetails[3])).build();
         request.setAttribute("user", user);
         filterChain.doFilter(request, response);
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
